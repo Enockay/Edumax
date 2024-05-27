db55f15021333542c2adcbcb5cc7f5267823a028
Enockay <enockaymwema@gmail.com>
2024-05-28 00:23:16 +0300

    added and modifed the teachers portal heading and dashboard

---
+++ frontend/teachersPortal/teachersportal/src/Header.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./css/Header.css";
+import teacher from './assets/teachers.webp'
+import profile from './assets/profile.png';
+
+const Header = () => {
+  const [menu, setMenu] = useState('');
+  const [menuItems] = useState(["Marks", "Units", "Students", "Classes"]);
+
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={teacher} alt="logo" className="logo-image" />
+        <p className="portal-title">TEACHERS PORTAL</p>
+      </div>
+      <div className="menu-search">
+        <select
+          id="menu-select"
+          value={menu}
+          onChange={(e) => setMenu(e.target.value)}
+          className="menu-select"
+        >
+          <option value="">--Select Item--</option>
+          {menuItems.map((item, index) => (
+            <option key={index} value={item}>{item}</option>
+          ))}
+        </select>
+        <button className="search-button">Search</button>
+      </div>
+      <div className="teacher-info">
+        <img src={profile} alt="profile" className="profile-photo" />
+        <span className="teacher-name">Mr. John Doe</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- frontend/teachersPortal/teachersportal/src/dashboard.jsx
+++ frontend/teachersPortal/teachersportal/src/dashboard.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import {React,useState,useEffect} from "react";
+import "./css/dashboard.css"
+import Header from "./Header";
 
 const Dashboard = () => {
-    return (
-        <div className="dashboard-container">
-            <h2>Dashboard</h2>
-            <p>Welcome to the Dashboard!</p>
-        </div>
-    );
-};
 
-export default Dashboard;
+    return(
+        <>
+        <div className="">
+            <Header/>
+        </div>
+        <div className="main">
+            <div className="sidebar">
+                 <center>I am  the sidebar</center>
+            </div>
+
+            <div className="main-content">
+              
+            </div>
+
+        </div>
+        <footer className="footer-dashboard">
+            <center>I am the footer</center>
+        </footer>
+        
+        </>
+    )
+}
+
+export default Dashboard
